50b021246f88738ba82edde555bc21d1daea56e0
fw <thatgloomyafternoon@gmail.com>
2026-03-14 08:39:56 +0700

    update

---
+++ src/main/java/com/fw/projecthq/Constants.java
@@ -0,0 +1,35 @@
+package com.fw.projecthq;
+
+public class Constants {
+
+  public static final String IMG_DEFAULT = "/images/img-default.png";
+  public static final String IMG_SUCCESS = "/images/img-success.png";
+  public static final String IMG_FAILED = "/images/img-failed.png";
+  public static final String[] TASK_LABELS = {
+      "Uncomplicated Firewall enabled",
+      "No incomplete package installation",
+      "No broken packages",
+      "ClamAV installed",
+      "Sublime Text installed",
+      "cURL installed",
+      "SDKMAN! installed",
+      "Java 17 & 21 installed",
+      "Git installed",
+      "Gradle installed",
+      "LibreOffice installed",
+      "GIMP installed",
+      "PostgreSQL 14 installed",
+      "DBeaver installed",
+      "Flameshot installed",
+      "net-tools installed",
+      "Chromium browser installed",
+      "VLC media player installed",
+      "Visual Studio Code installed",
+      "Tailscale installed",
+      "NodeJS + npm installed",
+      "Android Studio installed",
+      "ePSXe installed",
+      "SimpleScreenRecorder installed"
+  };
+
+}
