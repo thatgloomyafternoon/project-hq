63cadfae2d6b73ed67d2c016b8505f68368f08af
fw <thatgloomyafternoon@gmail.com>
2026-03-29 00:19:08 +0700

    update

--- src/main/java/com/fw/projecthq/tasks/Dbeaver.java
+++ src/main/java/com/fw/projecthq/tasks/Dbeaver.java
@@ -1,26 +1,26 @@
-package com.fw.projecthq.tasks;
-
-import com.fw.projecthq.CommandExecutor;
-import com.fw.projecthq.Constants;
-
-public class Dbeaver extends Task {
-
-  public Dbeaver(String label, CommandExecutor commandExecutor) {
-    super(label, commandExecutor);
-  }
-
-  @Override
-  public void execute(String password) {
-    commandExecutor.setCommands("/bin/bash", "-c",
-        "echo \"" + password + "\" | sudo -S /usr/bin/dpkg -l | grep dbeaver");
-    String response = commandExecutor.run();
-    boolean exists = response.contains("dbeaver-ce");
-    if (exists) {
-      setImage(Constants.IMG_SUCCESS);
-    } else {
-      setImage(Constants.IMG_FAILED);
-    }
-    repaint();
-  }
-
-}
+//package com.fw.projecthq.tasks;
+//
+//import com.fw.projecthq.CommandExecutor;
+//import com.fw.projecthq.Constants;
+//
+//public class Dbeaver extends Task {
+//
+//  public Dbeaver(String label, CommandExecutor commandExecutor) {
+//    super(label, commandExecutor);
+//  }
+//
+//  @Override
+//  public void execute(String password) {
+//    commandExecutor.setCommands("/bin/bash", "-c",
+//        "echo \"" + password + "\" | sudo -S /usr/bin/dpkg -l | grep dbeaver");
+//    String response = commandExecutor.run();
+//    boolean exists = response.contains("dbeaver-ce");
+//    if (exists) {
+//      setImage(Constants.IMG_SUCCESS);
+//    } else {
+//      setImage(Constants.IMG_FAILED);
+//    }
+//    repaint();
+//  }
+//
+//}
